eb9380c9687060c3aa292f0eab1c35cfc4007a52
Brendan Hawk <brendanhawk03@aol.com>
2025-11-13 08:44:37 -0500

    Setting up API connection. Not fully complete yet

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const searchInput = document.getElementById("search-input");
+const searchInput = document.getElementById("search-input").value;
 const searchBtn = document.getElementById("search-btn");
 const resultContainer = document.getElementById("result-container");
+
+searchBtn.addEventListener("click", async () => {
+	if (!searchInput) {
+		return alert("An error has occurred. Please select a different city.");
+	}
+
+	try {
+		const response = await fetch(`https://api.weather.gov/points/{latitude},{longitude}`);
+	} catch (error) {}
+});
