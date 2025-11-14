87864f43db2cf5d3e27edb0e04445250bd9acaf5
Brendan Hawk <brendanhawk03@aol.com>
2025-11-14 08:39:57 -0500

    Set up more of the api structure to display data to the DOM

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const searchInput = document.getElementById("search-input").value;
 const searchBtn = document.getElementById("search-btn");
 const resultContainer = document.getElementById("result-container");
 
+//Set up the event listener to the api
+//Check if there is any data input
+//Take city and convert it to longitute and latitude
+//Pass the Long. and Lat. to the National Weather Service api
+//Display the forcast to the DOM
+
 searchBtn.addEventListener("click", async () => {
 	if (!searchInput) {
 		return alert("An error has occurred. Please select a different city.");
@@ -9,5 +15,15 @@ searchBtn.addEventListener("click", async () => {
 
 	try {
 		const response = await fetch(`https://api.weather.gov/points/{latitude},{longitude}`);
+
+		if (!response.ok) {
+			throw new Error("Could not get the data");
+		}
+
+		const data = response.json();
+
+		resultContainer.innerHTML = data.Search.map(() => {
+			console.log(data);
+		});
 	} catch (error) {}
 });
